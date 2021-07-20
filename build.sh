65f4bfa5a50582ea7c4e98e968edcd19d68dc11a
Rogerio Machado <rogermm@gmail.com>
2021-07-20 16:44:51 -0300

    Update

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-#docker build --no-cache -t rogermm/command:1.0 .
-docker build $1 -t  rogermm/command:1.0 .
+docker build --no-cache -t rogermm/command:1.0 .
+#docker build $1 -t  rogermm/command:1.0 .
