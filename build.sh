16db4c0581c418e34c7c8aaa91420bef5d8bcd2e
Rogerio Machado <rogermm@gmail.com>
2021-08-05 09:59:52 -0300

    Update

--- build.sh
+++ build.sh
@@ -1,2 +1,34 @@
-docker build --no-cache -t rogermm/command:1.0 .
-#docker build $1 -t  rogermm/command:1.0 .
+#!/bin/bash
+
+set -e
+
+TAG=$(git rev-parse --abbrev-ref HEAD)
+USER=$USERNAME
+
+build() {
+    NAME=$1
+    PREFIX=$2
+
+    if [ "$PREFIX" = "" ]; then
+      IMAGE=$USER/$NAME:$TAG
+    else
+      IMAGE=$USER/$PREFIX-$NAME:$TAG
+    fi
+    cd $([ -z "$4" ] && echo "./$NAME" || echo "$4")
+    echo '--------------------------' building $IMAGE in $(pwd)
+    echo "docker build -t $IMAGE --build-arg USERNAME=$1 --build-arg TAG=$TAG ."
+    docker build -t $IMAGE --build-arg USERNAME=$1 --build-arg TAG=$TAG .
+    cd -
+}
+
+while getopts u:a:f: flag
+do
+    case "${flag}" in
+        u) USER=${OPTARG};;
+        *);;
+    esac
+done
+
+build command
+build tinyproxy
+
---
+++ exec-traefik.sh
@@ -0,0 +1,1 @@
+docker-compose exec traefik /bin/sh
